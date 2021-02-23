3f0c24ec8681da9898c5ae6dcb48354adea66fff
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-02-23 14:09:07 -0500

    reformat

--- routes/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-// app.get("/exercise", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "./public/exercise.html"));
-// });
-
-// app.get("/stats", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "./public/stats.html"));
-// });
-
