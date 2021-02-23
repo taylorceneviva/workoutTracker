4d302248bce3676f4efad516a276bb60d5390fda
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-02-23 14:37:57 -0500

    deploying

--- server.js
+++ server.js
@@ -14,11 +14,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Routes-html
 app.get("/", (req, res) => {
